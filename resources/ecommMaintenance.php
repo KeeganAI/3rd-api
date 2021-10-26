339a54a907acafc8dc31c34515fddb8f8f0cb173
KeeganAI <53181748+KeeganAI@users.noreply.github.com>
2021-10-26 22:25:41 +0200

    update

---
+++ resources/ecommMaintenance.php
@@ -0,0 +1,49 @@
+
+                <img src="https://raw.githubusercontent.com/KeeganAI/3rd-api/main/assets/maintenance.png" class="img-fluid w-75">
+                <h3 class="fw-600 mt-5">{{translate('We are Under Maintenance.')}}</h3>
+                <div class="lead">{{translate('We will be back soon!')}}</div>
+                <style>
+                    .w-75 {
+                        width: 75%!important;
+                    }
+                    .img-fluid {
+                        max-width: 100%;
+                        height: auto;
+                    }
+
+                    .fw-600 {
+                        font-weight: 600 !important;
+                    }
+                    .mt-5 {
+                        margin-top: 3rem !important;
+                    }
+                    h3 {
+                        font-size: 1.75rem;
+                    }
+                    h3 {
+                        margin-bottom: 0.5rem;
+                        font-weight: 500;
+                        line-height: 1.2;
+                    }
+                    h3 {
+                        margin-top: 0;
+                        margin-bottom: 0.5rem;
+                    }
+                    *, ::after, ::before {
+                        box-sizing: border-box;
+                    }
+
+                    h3 {
+                        display: block;
+                        font-size: 1.17em;
+                        margin-block-start: 1em;
+                        margin-block-end: 1em;
+                        margin-inline-start: 0px;
+                        margin-inline-end: 0px;
+                        font-weight: bold;
+                    }
+                    .lead {
+                        font-size: 1.25rem;
+                        font-weight: 300;
+                    }
+                </style>
