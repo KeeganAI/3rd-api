a4772bf92bc9bcf2d1acee9639b254ce7be6bfd4
KeeganAI <53181748+KeeganAI@users.noreply.github.com>
2021-11-15 16:14:57 +0100

    file update
    Creata funzione messaggio aggiornamento.

---
+++ resources/update.php
@@ -0,0 +1,26 @@
+<div class="alert alert-success" role="alert">
+<strong>Nuova versione disponibile per il tuo sito <?php $res['version']; ?> contatta Keegan#5295 | lrnz.xyz | kgn.ovh se hai bisogno di aiuto.</strong>
+</button>
+</div>
+
+<style>
+
+.alert {
+ position:relative;
+ padding:.75rem 1.25rem;
+ margin-bottom:1rem;
+ border:1px solid transparent;
+ border-radius:.35rem
+}
+
+.alert-success {
+ color:#0f6848;
+ background-color:#d2f4e8;
+ border-color:#bff0de
+}
+
+.alert-dismissible {
+ padding-right:4rem
+}
+
+</style>
